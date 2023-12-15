f97121faf8a60787eccab27c041e25125d74eea8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-15 14:48:32 +0530

    frequency of each element in an array or a list

---
+++ src/main/java/com/bean/string/StringFrequ.java
@@ -0,0 +1,10 @@
+// How do you find frequency of each element in an array or a list?
+package com.bean.string;
+
+public class StringFrequ
+{
+    public static void main(String[] args)
+    {
+        System.out.println("****************");
+    }
+}
