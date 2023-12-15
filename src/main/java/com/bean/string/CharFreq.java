e7a720200afeb891678c835814f7ddabe8b90111
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-15 15:03:35 +0530

    String::toLerCase

---
+++ src/main/java/com/bean/string/CharFreq.java
@@ -0,0 +1,22 @@
+package com.bean.string;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CharFreq
+{
+    static void charFreq(String st)
+    {
+        Arrays.stream(st.split("")).map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet()
+                .forEach(System.out::print);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("/n Character frequency using Java8");
+        charFreq("String");
+    }
+}
