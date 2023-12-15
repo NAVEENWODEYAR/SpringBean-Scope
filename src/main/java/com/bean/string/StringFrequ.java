ccf97402f37805100c7b72a162b6a6837aec25f5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-15 14:53:00 +0530

    Function.identity()

--- src/main/java/com/bean/string/StringFrequ.java
+++ src/main/java/com/bean/string/StringFrequ.java
@@ -1,10 +1,22 @@
 // How do you find frequency of each element in an array or a list?
 package com.bean.string;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class StringFrequ
 {
+    static void strFreq(List<String> list)
+    {
+            list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().forEach(System.out::println);
+    }
+
     public static void main(String[] args)
     {
         System.out.println("****************");
+
+        strFreq(Arrays.asList("One","Three","Two","One","Three"));
     }
 }
