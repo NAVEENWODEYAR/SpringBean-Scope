f9f18d56ec09692c0bc3d7bd023009f8b5f63693
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-15 12:36:14 +0530

    anagram string

---
+++ src/main/java/com/bean/string/Anagram.java
@@ -0,0 +1,11 @@
+package com.bean.string;
+
+public class Anagram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\n two strings are considered anagrams if they have the same characters with the same frequency, " +
+                "but the order of the characters may be different.");
+
+    }
+}
