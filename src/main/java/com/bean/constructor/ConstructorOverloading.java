a0acc707b8986a67b0d68a3bd0cd65241faae3a9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-16 20:41:20 +0530

    constructor overloading

---
+++ src/main/java/com/bean/constructor/ConstructorOverloading.java
@@ -0,0 +1,18 @@
+package com.bean.constructor;
+
+public class ConstructorOverloading
+{
+    ConstructorOverloading()
+    {
+        System.out.println("Constructor:1");
+    }
+    ConstructorOverloading(int a)
+    {
+        System.out.println("Constructor:2");
+    }
+    public static void main(String[] args)
+    {
+        new ConstructorOverloading();
+        new ConstructorOverloading(1);
+    }
+}
