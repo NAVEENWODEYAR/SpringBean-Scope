6bc3e6847b7f044b621483decd398aefa0dc230f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-17 23:55:23 +0530

    aggregration

---
+++ src/main/java/com/bean/inherit/B.java
@@ -0,0 +1,12 @@
+package com.bean.inherit;
+
+public class B
+{
+    private final static int a=0;
+    private static final String st="";
+
+    void t1()
+    {
+        System.out.println("\n Method:class");
+    }
+}
