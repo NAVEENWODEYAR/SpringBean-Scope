e465021c8a381543ac1721befae03dc63cc8126d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-18 20:50:53 +0530

    default values

---
+++ src/main/java/com/bean/inherit/D.java
@@ -0,0 +1,21 @@
+package com.bean.inherit;
+
+import java.lang.reflect.Array;
+
+public final class D
+{
+    static int i;
+    static float f;
+    static boolean b;
+    static char ch;
+    static Array[] ar;
+
+    public static void main(String[] args)
+    {
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(ar);
+    }
+}
