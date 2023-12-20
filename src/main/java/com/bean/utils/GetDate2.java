ebe4cd3f1455cf951ba69850f48bb1f354d0fdd9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-20 22:29:28 +0530

    logOut

--- src/main/java/com/bean/utils/GetDate2.java
+++ src/main/java/com/bean/utils/GetDate2.java
@@ -2,6 +2,7 @@ package com.bean.utils;
 
 import org.springframework.cglib.core.Local;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -9,11 +10,21 @@ import java.util.Date;
 public class GetDate2 {
     static void getDate()
     {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("xx:xx:xx HH:MM:SS");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:MM:SS");
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(dtf.format(localDateTime));
     }
+
+    static void getDate1()
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        System.out.println(formatter.format(date));
+    }
+
     public static void main(String[] args) {
         getDate();
+        System.out.println("*************");
+        getDate1();
     }
 }
