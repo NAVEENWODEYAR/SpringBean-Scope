20c33cb801cbaf270423fe1651cf207e710b3473
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-20 22:02:21 +0530

    LocalDate

---
+++ src/main/java/com/bean/utils/GetDate.java
@@ -0,0 +1,12 @@
+package com.bean.utils;
+
+public class GetDate {
+    static void getDate()
+    {
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
