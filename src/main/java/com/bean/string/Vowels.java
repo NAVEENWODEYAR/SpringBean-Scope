51dfdbca3288e65dbea4f7fe3d45f369780e49e4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-21 23:55:29 +0530

    pattern matching

--- src/main/java/com/bean/string/Vowels.java
+++ src/main/java/com/bean/string/Vowels.java
@@ -2,8 +2,7 @@ package com.bean.string;
 
 public class Vowels
 {
-    public boolean strContainsVowels(String st)
-    {
+    public boolean strContainsVowels(String st){
         return st.toLowerCase().matches(".*[aeiou].*");
     }
 }
