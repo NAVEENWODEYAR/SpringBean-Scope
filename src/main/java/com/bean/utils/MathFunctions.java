6ae457906047f717070775a39505ed054642c2ad
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-21 07:48:30 +0530

    GM

---
+++ src/main/java/com/bean/utils/MathFunctions.java
@@ -0,0 +1,9 @@
+package com.bean.utils;
+
+public class MathFunctions {
+
+    public static void main(String[] args) {
+        System.out.println("Math class");
+        System.out.println(Math.sqrt(56));
+    }
+}
