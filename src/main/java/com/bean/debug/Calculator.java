ceb7702fe6194482f6dc1d3400c0dab40c7cb561
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-22 23:53:42 +0530

    return type of the function

--- src/main/java/com/bean/debug/Calculator.java
+++ src/main/java/com/bean/debug/Calculator.java
@@ -2,13 +2,7 @@ package com.bean.debug;
 
 public class Calculator {
 
-    int add(int a,int b)
-    {
-        return a+b;
-    }
-
-    double sub(int a, int b)
-    {
-        return a-b;
-    }
+    int add(int a,int b) {return a+b;}
+    float prod(int a, int b){return a*b;}
+    double sub(int a, int b){return a-b;}
 }
