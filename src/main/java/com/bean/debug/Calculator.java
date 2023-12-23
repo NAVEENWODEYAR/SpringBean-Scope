cebf0327e0a371b27ee0beadca3fade2bf92eb1a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-23 10:13:21 +0530

    Long

--- src/main/java/com/bean/debug/Calculator.java
+++ src/main/java/com/bean/debug/Calculator.java
@@ -5,4 +5,5 @@ public class Calculator {
     int add(int a,int b) {return a+b;}
     float prod(int a, int b){return a*b;}
     double sub(int a, int b){return a-b;}
+    long mul(int a, int b){return a/b;}
 }
