778d050d36b237894e340a4e89b43b68a03924aa
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-23 10:26:18 +0530

    Thread.start()

--- src/main/java/com/bean/array/threads/ThreadEx.java
+++ src/main/java/com/bean/array/threads/ThreadEx.java
@@ -3,11 +3,13 @@ package com.bean.array.threads;
 public class ThreadEx extends Thread {
     public void run()
     {
-        System.out.println("\n Thread,");
+        System.out.println(" Thread running");
+        for (int i=0; i<=5;i++)
+            System.out.println("Thread pool,"+i);
     }
 
     public static void main(String[] args) {
-        ThreadEx t = new ThreadEx();
+        Thread t = new ThreadEx();
                     t.start();
         System.out.println("\n Main method,");
     }
