711260cbe57e1608b952b695dbfde73c5eff4971
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-23 10:36:05 +0530

    Thread Concurency problems

--- src/main/java/com/bean/array/threads/ThreadEx.java
+++ src/main/java/com/bean/array/threads/ThreadEx.java
@@ -4,13 +4,14 @@ public class ThreadEx extends Thread {
     public void run()
     {
         System.out.println(" Thread running");
-        for (int i=0; i<=5;i++)
-            System.out.println("Thread pool,"+i);
+        a++;
     }
-
+    static int a;
     public static void main(String[] args) {
         Thread t = new ThreadEx();
                     t.start();
-        System.out.println("\n Main method,");
+        System.out.println(a);
+        a++;
+        System.out.println(a);
     }
 }
