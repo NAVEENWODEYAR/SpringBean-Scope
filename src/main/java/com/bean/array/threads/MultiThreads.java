d5291d79aa8e9ba2e8b99d955f001de34e487700
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-23 10:51:27 +0530

    Thread.currentThread().getId()

---
+++ src/main/java/com/bean/array/threads/MultiThreads.java
@@ -0,0 +1,20 @@
+package com.bean.array.threads;
+
+public class MultiThreads extends Thread {
+    public void run()
+    {
+        try{
+            System.out.println("\n Thread "+Thread.currentThread().getId());
+        }catch (Exception e){
+            System.out.println(e.getCause());
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("\n Multithreading is a Java feature that allows concurrent execution of two or more parts of a program for maximum utilization of CPU");
+        for (int i=0; i<10; i++)
+        {
+            MultiThreads ml = new MultiThreads();
+                        ml.start();
+        }
+    }
+}
--- src/main/java/com/bean/array/threads/ThreadEx.java
+++ src/main/java/com/bean/array/threads/ThreadEx.java
@@ -4,14 +4,9 @@ public class ThreadEx extends Thread {
     public void run()
     {
         System.out.println(" Thread running");
-        a++;
     }
-    static int a;
     public static void main(String[] args) {
         Thread t = new ThreadEx();
                     t.start();
-        System.out.println(a);
-        a++;
-        System.out.println(a);
     }
 }
