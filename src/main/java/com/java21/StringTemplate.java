b660bb306218ccf37722066c1283862d26145c3a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-25 12:39:53 +0530

    String Template from Java21

---
+++ src/main/java/com/java21/StringTemplate.java
@@ -0,0 +1,16 @@
+package com.java21;
+
+import static java.lang.StringTemplate.STR;
+
+public class StringTemplate {
+    public static void main(String[] args) {
+        System.out.println("StringTemplate in java 21");
+        int a = 0;
+        int b = 1;
+        String st = "String Template";
+
+        System.out.println(STR."value of a:\{a}");
+        System.out.println(STR."value of b:\{b}");
+        System.out.println(STR."value of st:\{st}");
+    }
+}
