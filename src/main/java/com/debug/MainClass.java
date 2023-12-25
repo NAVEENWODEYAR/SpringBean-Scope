3b6ccb520434748bef95d5c09ca4cf04f8fb1ff5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-25 12:35:24 +0530

    java 21 preview features

--- src/main/java/com/debug/MainClass.java
+++ src/main/java/com/debug/MainClass.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("***********");
-        var c = new Calculator();
+        var c = new Calculator();   
         int a = 1;
         int b = 3;
 
