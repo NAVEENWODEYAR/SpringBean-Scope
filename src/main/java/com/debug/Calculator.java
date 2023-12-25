e1c96d9e3608c72d7c62347395e130f05effd547
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-25 09:56:55 +0530

    LogIn

---
+++ src/main/java/com/debug/Calculator.java
@@ -0,0 +1,14 @@
+package com.debug;
+
+public class Calculator {
+    public int add(int a, int b){
+        System.out.println("Addition,");
+        return a+b;
+    }
+
+    public int dif(int a, int b){
+        System.out.println("Difference,");
+        return a-b;
+    }
+
+}
