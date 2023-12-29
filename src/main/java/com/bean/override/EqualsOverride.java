4f5d28b78436290cfc8b15cee4e4d8defc511da5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-29 22:57:26 +0530

    overriding equals()

--- src/main/java/com/bean/override/EqualsOverride.java
+++ src/main/java/com/bean/override/EqualsOverride.java
@@ -1,18 +1,27 @@
 package com.bean.override;
 
 public class EqualsOverride {
+    private int i;
+    private String s;
+
+    public EqualsOverride(int i,String s) {
+        this.i = i;
+        this.s = s;
+    }
 
     @Override
     public boolean equals(Object obj) {
         EqualsOverride e = (EqualsOverride) obj;
         EqualsOverride e1 = (EqualsOverride) obj;
 
-        return e.equals(e1);
+        if(e.i == e1.i || e.s ==e1.s)
+        return true;
+        return false;
     }
 
     public static void main(String[] args) {
-        EqualsOverride ob = new EqualsOverride();
-        EqualsOverride ob1 = new EqualsOverride();
+        EqualsOverride ob = new EqualsOverride(1,"1");
+        EqualsOverride ob1 = new EqualsOverride(1,"1");
 
         if (ob.equals(ob1))
             System.out.println("Equals method overridden");
